af5e1caef856cc63c08fcc37d13e5b9ec5357601
Gennady Verdel <godrose@gmail.com>
2019-12-06 13:09:02 +0200

    feat(bitmap-islands): implementing correct render flow

--- src/app.ts
+++ src/app.ts
@@ -10,43 +10,43 @@ export class App {
   attached() {
     const canvas:any = document.getElementById('myCanvas');
     this.context = canvas.getContext('2d');
+    this.width = 1000;
+    this.height = 1000;
+    this.populateData();
     this.render();
 
     canvas.addEventListener('click', evt => {      
       var mousePos = this.getSquare(canvas, evt);
-      this.data[mousePos.x][mousePos.y].isMarked = true;    
+      this.data[mousePos.x][mousePos.y].isMarked = true;  
+      this.render();  
 }, false);
   }
 
   generate() {  
-    this.populateData();
-    const canvas:any = document.getElementById('myCanvas');
-    this.drawGridOnCanvas(canvas); 
+    this.populateData();    
+    this.drawGrid(); 
   }
 
   private populateData() {
-    this.data = new Pixel[this.height][this.width];
+    this.data = new Array<Array<Pixel>>();
+    for (let i = 0; i < this.height; i++) {
+      let arr = new Array<Pixel>();      
+      for (let j = 0; j < this.width; j++) {
+        arr.push(new Pixel(i, j));
+      }      
+      this.data.push(arr);
+    }    
+  }
+  
+  private render() {
+    this.drawGrid();
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        if (this.data[i][j]) {
+        if (this.data[i][j].isMarked) {
           this.fillSquare(i, j);
         }        
       }      
-    }
-  }
-
-  private drawGridOnCanvas(canvas) : any {        
-    this.drawGrid();    
-  }
-
-  private render() {
-    this.drawGrid()
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        const element = new Pixel(i, j);
-        this.data[i][j] = element;
-      }      
-    }
+    }    
     requestAnimationFrame(this.render);
   }
 
@@ -61,7 +61,7 @@ export class App {
   private drawGrid() {
     for (var x = 0.5; x < 10001; x += 10) {
       this.context.moveTo(x, 0);
-      this.context.lineTo(x, 10000);
+        this.context.lineTo(x, 10000);
     }
     
     for (var y = 0.5; y < 10001; y += 10) {
@@ -77,9 +77,4 @@ export class App {
     this.context.fillStyle = "gray"
     this.context.fillRect(x,y,9,9);
   }
-
 }
-
-
-
-
