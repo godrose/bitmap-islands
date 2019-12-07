865c08e80eb58ccc10a93fd15ddbfc96dfae0f67
Gennady Verdel <godrose@gmail.com>
2019-12-07 09:23:46 +0200

    minor: fixed compilation error

--- src/pixel.ts
+++ src/pixel.ts
@@ -1,14 +1,10 @@
 export class Pixel {
   
-  public row: number;
-  public col: number;
   public x: number;
   public y: number;
   public isMarked: boolean;
 
-  constructor(row: number, col: number, x: number, y: number){
-    this.row = row;
-    this.col = col;
+  constructor(x: number, y: number){
     this.x = x;
     this.y = y;
   } 
